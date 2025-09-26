80e5b45e281127222ae97ef9bc0ba985642e4ddb
NannaWH <nanna_emilie@hotmail.com>
2025-09-26 14:19:04 -0700

    Updated power out function to include error messages if invalid inputs are entered into the function

--- main.py
+++ main.py
@@ -6,8 +6,18 @@
 
 ## We define the power function
 def power_calc(v, p_rated=15, v_in = 3, v_ratd = 11, v_out = 25, interpol = "linear"):
-    """We define a function that calculates power output based on user inputs"""
+    """We define a function which calculates power output based on user inputs"""
     
+    # We check for error in user input 
+    inputs = {'v': v, 'p_rated': p_rated, 'v_in': v_in, 'v_ratd': v_ratd, 'v_out': v_out}
+
+    for name, value in inputs.items():
+        if not isinstance(value, (int, float)): 
+            raise ValueError(f"Invalid input: {value} is not a number")  
+    
+    if not interpol == "linear" or interpol == "cubic":
+        raise ValueError(f"Invalid input: {interpol}. Interpolation should be either 'linear' or 'cubic'.")  
+
     # We define g(v)
     if interpol == "linear":
         g = (v-v_in)/(v_ratd-v_in)
@@ -24,8 +34,9 @@ def power_calc(v, p_rated=15, v_in = 3, v_ratd = 11, v_out = 25, interpol = "lin
         
     return p
 
+#We use the function to calculate and print the power output
 if __name__ == '__main__':
-    # Write the main script to use the function here:
+    # We call the power output function to calculate power output based on user defined inputs:
     power = power_calc(10, 15, 3, 11, 25, "linear")
 
     # We print the calculated power output: 
