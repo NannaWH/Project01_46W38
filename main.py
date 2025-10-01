caaff88ee076fc2dc86c877d9b8db60492db119a
NannaWH <nanna_emilie@hotmail.com>
2025-10-01 07:51:44 -0700

    Updating code with more description to make it more user friendly and easier to follow

--- main.py
+++ main.py
@@ -4,11 +4,11 @@
 
 ## This file includes the functions and code to solve project 1
 
-## We define the power function
+## We define a function for the power curve
 def power_calc(v, p_rated=15, v_in = 3, v_ratd = 11, v_out = 25, interpol = "linear"):
-    """We define a function which calculates power output based on user inputs"""
+    """We define a function that calculates power output based on user inputs"""
     
-    # We check for error in user input 
+    # We check for errors in user input and raise errors if any
     inputs = {'v': v, 'p_rated': p_rated, 'v_in': v_in, 'v_ratd': v_ratd, 'v_out': v_out}
 
     for name, value in inputs.items():
@@ -18,26 +18,26 @@ def power_calc(v, p_rated=15, v_in = 3, v_ratd = 11, v_out = 25, interpol = "lin
     if not interpol == "linear" or interpol == "cubic":
         raise ValueError(f"Invalid input: {interpol}. Interpolation should be either 'linear' or 'cubic'.")  
 
-    # We define g(v)
+    # We define g(v) based on linear or cubic interpolation assumption
     if interpol == "linear":
         g = (v-v_in)/(v_ratd-v_in)
     elif interpol == "cubic":
         g = v**3/v_ratd**3
 
-    # We calculate the power output P(v)
+    # We calculate the power output P(v) based on power curve assumptions
     if v < v_in or v >= v_out:
         p = 0
     elif v_in <= v < v_ratd:
         p = g*p_rated
-    else: 
+    elif v_ratd <= v < v_out:
         p = p_rated
         
     return p
 
-#We use the function to calculate and print the power output
+#We use the power function to calculate and print the power output
 if __name__ == '__main__':
     # We call the power output function to calculate power output based on user defined inputs:
-    power = power_calc(10, 15, 3, 11, 25, "linear")
+    power = power_calc(10, 15, 4, 12, 25, "linear")
 
     # We print the calculated power output: 
     print(f"Power Output is: {power:.2f} MW")
